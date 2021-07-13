63941f85d8b0a95d15ba90f18cfa099482c0a1fc
bogoreh <76124689+bogoreh@users.noreply.github.com>
2021-07-13 16:06:17 +0300

    The first commit

---
+++ 06_Interactive Map/00_HOW TO BUILD INTERACTIVE MAP PROJECT  USING JAVASCRIPT/js/index.js
@@ -0,0 +1,17 @@
+function ("const".button.easybutton) {
+    let
+    const = function.button(const.continue);
+    let
+
+    function = document.querySelector('function');
+    let button = function ('continue');
+
+    button.addEventListener('function',
+        continue) {
+        button.add.them.function(const.button)
+        floor.random(
+            continue.button.const.function)
+        add.function.continue.function.floor.random
+        continue.button.add.then()
+    }
+}
